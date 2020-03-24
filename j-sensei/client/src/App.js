508cc01a3de5eaad6e1982352e5aa43a817d5050
seriousdave42 <seriousdave42@gmail.com>
2020-03-23 20:27:05 -0500

    crude lesson layout

--- j-sensei/client/src/App.js
+++ j-sensei/client/src/App.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import { Router } from '@reach/router'; 
-
-
-//remember to import axios and  @react/router for {Link}
+import { Router } from '@reach/router';
+import Dashboard from './views/Dashboard';
+import Lesson from './views/Lesson'; 
 
 function App() {
+
+  const [userScores, setUserScores] = useState([
+    {
+      lessonName: "Lesson 1",
+      highScore: 0,
+      lessonColor: "danger"
+  },
+  {
+      lessonName: "Lesson 2",
+      highScore: 0,
+      lessonColor: "danger"
+  },
+  {
+      lessonName: "Lesson 3",
+      highScore: 0,
+      lessonColor: "danger"
+  }
+  ])
+  
+  const scoreUpdate = (lesson, score) => {
+    let newColor = "";
+  
+    if(score <= 1) {
+      newColor = "danger"
+    }
+    else if(score < 3) {
+      newColor = "warning"
+    }
+    else {
+      newColor = "success"
+    }
+  
+    const newScore = {
+      lessonName: `Lesson ${lesson}`,
+      highScore: score,
+      lessonColor: newColor
+    }
+  
+    setUserScores([...userScores.slice(0, lesson-1), newScore, ...userScores.slice(lesson)])
+  }
+
   return (
     <div className="App">
+        <h1>Welcome to J-Sensei!</h1>
         <Router>
-          <Main path='/' />
+          <Dashboard path='/' userScores={userScores}/>
+          <Lesson path='/lesson/:id' scoreUpdate={scoreUpdate}/>
       </Router>
     </div>
   );
