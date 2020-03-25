09450b52b6b81a01c87cbcf9aa65fabc8b54f5e9
Dan <e.danik90@gmail.com>
2020-03-25 17:59:25 -0500

    small fixes/ added dropdown menu to navbar

--- j-sensei/client/src/components/NavBar.js
+++ j-sensei/client/src/components/NavBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Link} from '@reach/router';
+import { Link } from '@reach/router';
+import Logout from './Logout';
 
-const NavBar = ({style, username}) => {
+const NavBar = ({ style, username }) => {
     return (
         <nav style={style.navbar} className="navbar navbar-expand-lg navbar-light">
             <Link className="navbar-brand mr-auto" to="/dashboard">J-Sensei!</Link>
             <Link className="navbar-brand mr-auto" to="/dashboard">Learn</Link>
-            <span className="navbar-text">Welcome, {username}!</span>
+            <div className="dropdown show">
+                <a className="navbar-brand dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Welcome, {username}
+                </a>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <Link className="dropdown-item" to="#">Account pofile</Link>
+                    <Logout />
+                </div>
+            </div>
         </nav>
     )
 }
--- j-sensei/client/src/views/Dashboard.js
+++ j-sensei/client/src/views/Dashboard.js
@@ -6,21 +6,24 @@ import {Link, navigate} from '@reach/router';
 const Dashboard = ({userScores, style}) => {
     const [hiraganas, setHiraganas] = useState([]);
     const [username, setUsername] = useState("");
+    const [isAuthorized, setIsAuthorized] = useState(false);
     useEffect(() => {
         Axios.get("http://localhost:8000/api/hiragana", { withCredentials: true })
             .then(res => {
+                setIsAuthorized(true);
                 setHiraganas(res.data);
                 setUsername(res.data.nickname);
             })
             .catch(err => navigate("/"))
-    }, []);
+    }, [hiraganas]);
     return (
-        <div>
-        <NavBar username={username} style={style}/>
-            {userScores.map((lesson, i) => 
-                <Link key={i} to={`/lesson/${i+1}`} className={`btn btn-${lesson.lessonColor} mx-2 ${i>0 && userScores[i-1].highScore<5 && "disabled"}`}>{lesson.lessonName}</Link>
-            )}
-        </div>
+        isAuthorized && 
+            <div>
+            <NavBar username={username} style={style}/>
+                {userScores.map((lesson, i) => 
+                    <Link key={i} to={`/lesson/${i+1}`} className={`btn btn-${lesson.lessonColor} mx-2 ${i>0 && userScores[i-1].highScore<5 && "disabled"}`}>{lesson.lessonName}</Link>
+                )}
+            </div>
     )
 }
 
